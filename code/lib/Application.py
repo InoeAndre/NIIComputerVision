TSDF Mesh but computation may be wrong

code/lib/Application.py
@@ -254,7 +254,7 @@ class Application(tk.Frame):
 #         self.RGBD2.Vmap_optimize()  
 #         self.RGBD2.NMap_optimize()
 #==============================================================================
-            
+        self.TSDF = np.zeros((512,512,512),np.float32)
         self.TSDFrendering = []
         nbfus = 3
         nbLabel = self.RGBD.bdyPart.shape[0]
@@ -281,7 +281,7 @@ class Application(tk.Frame):
                 
                 # surface rendering TSDF
                 TSDFManager = TSDFtk.TSDFManager((512,512,512), self.RGBD_TSDF, self.GPUManager)
-                TSDFManager.FuseRGBD_GPU(self.RGBD_TSDF, self.Pose)          
+                self.TSDF += TSDFManager.FuseRGBD_GPU(self.RGBD_TSDF, self.Pose)          
                 # new surface prediction          
                 self.RGBD.depth_image +=  TSDFManager.RayTracing_GPU(self.RGBD_TSDF, self.Pose)
 
@@ -308,7 +308,7 @@ class Application(tk.Frame):
 
         #TSDFrendering = self.DrawColors2D(TSDFrendering,self.Pose)
         # Extract the 100-isosurface
-        vertices1, triangles1 = mcubes.marching_cubes(self.TSDFrendering[-1], 100)
+        vertices1, triangles1 = mcubes.marching_cubes(self.TSDF, 100)
         img = Image.fromarray(self.TSDFrendering[-1], 'RGB')
         self.imgTk=ImageTk.PhotoImage(img)
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.imgTk)

code/lib/TSDF.py
@@ -33,8 +33,8 @@ class TSDFManager():
     def __init__(self, Size, Image, GPUManager):
         self.Size = Size
         self.TSDF = np.zeros(self.Size, dtype = np.float32)
-        self.prevTSDF = np.zeros(self.Size, dtype = np.float32)
-        self.Weight = np.ones(self.Size, dtype = np.float32)
+        #self.prevTSDF = np.zeros(self.Size, dtype = np.float32)
+        #self.Weight = np.ones(self.Size, dtype = np.float32)
         self.c_x = self.Size[0]/2
         self.c_y = self.Size[1]/2
         self.c_z = -0.1
@@ -46,8 +46,8 @@ class TSDFManager():
         self.Size_Volume = cl.Buffer(self.GPUManager.context, mf.READ_ONLY | mf.COPY_HOST_PTR, \
                                hostbuf = np.array([self.Size[0], self.Size[1], self.Size[2]], dtype = np.int32))
         self.TSDFGPU = cl.Buffer(self.GPUManager.context, mf.READ_WRITE, self.TSDF.nbytes)
-        self.prevTSDFGPU = cl.Buffer(self.GPUManager.context, mf.READ_WRITE, self.prevTSDF.nbytes)
-        self.WeightGPU = cl.Buffer(self.GPUManager.context, mf.READ_WRITE, self.Weight.nbytes)
+        #self.prevTSDFGPU = cl.Buffer(self.GPUManager.context, mf.READ_WRITE, self.prevTSDF.nbytes)
+        #self.WeightGPU = cl.Buffer(self.GPUManager.context, mf.READ_WRITE, self.Weight.nbytes)
         self.Param = cl.Buffer(self.GPUManager.context, mf.READ_ONLY | mf.COPY_HOST_PTR, \
                                hostbuf = np.array([self.c_x, self.dim_x, self.c_y, self.dim_y, self.c_z, self.dim_z], dtype = np.float32))
         
@@ -70,10 +70,15 @@ class TSDFManager():
         cl.enqueue_write_buffer(self.GPUManager.queue, self.Pose_GPU, Transform)
         cl.enqueue_write_buffer(self.GPUManager.queue, self.DepthGPU, Image.depth_image)
         
-        self.prevTSDFGPU = self.TSDFGPU
+        #self.prevTSDFGPU = self.TSDFGPU
+        #add this in the functionself.prevTSDFGPU, self.WeightGPU,
         self.GPUManager.programs['FuseTSDF'].FuseTSDF(self.GPUManager.queue, (self.Size[0], self.Size[1]), None, \
-                                self.TSDFGPU,self.prevTSDFGPU, self.WeightGPU, self.DepthGPU, self.Param, self.Size_Volume, self.Pose_GPU, self.Calib_GPU, \
+                                self.TSDFGPU, self.DepthGPU, self.Param, self.Size_Volume, self.Pose_GPU, self.Calib_GPU, \
                                 np.int32(Image.Size[0]), np.int32(Image.Size[1]))
+        
+        cl.enqueue_read_buffer(self.GPUManager.queue, self.TSDFGPU, self.TSDF).wait()
+        
+        return self.TSDF
         
     # Reay tracing on the GPU
     def RayTracing_GPU(self, Image, Pose):